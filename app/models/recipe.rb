d23cb8d7434ae0b7a52fa7cf7cccca99b7d8e08c
EC2 Default User <ec2-user@ip-172-31-47-102.ap-northeast-1.compute.internal>
2023-02-21 10:32:46 +0000

    test

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,12 +4,14 @@ class Recipe < ApplicationRecord
   belongs_to :genre
   has_one_attached :image
   
-  has_many :recipe_comments, dependent: :destroy
+  has_many :recipe_comments, dependent: :destroy 
   has_many :favorites, dependent: :destroy
   
+
   def favorited_by?(customer)
     favorites.exists?(customer_id: customer.id)
   end
+
   
 
   def get_image
